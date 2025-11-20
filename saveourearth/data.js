8ae7f8e3732e15afbb346a093066beab3a854223
Mariela Matute <mat20007@byui.edu>
2025-11-20 13:03:54 -0700

    Delete saveourearth/data.js

--- saveourearth/data.js
+++
@@ -1,7 +0,0 @@
-export const tips = [
-    { tip: "Use reusable bags instead of plastic.", impact: 2 },
-    { tip: "Turn off lights when not in use.", impact: 1 },
-    { tip: "Recycle paper, glass, and metal.", impact: 2 },
-    { tip: "Bike or walk instead of driving.", impact: 3 },
-    { tip: "Plant trees or grow plants at home.", impact: 3 }
-];
